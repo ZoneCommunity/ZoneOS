2aa362e73fafa93f764d0215fc701cd688756883
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-26 00:47:26 -0400

    Current files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ZoneOS
+
+An operating system.
+
+## Planned Features:
+### Currently
+- [ ] IDT
+- [x] Keyboard input
+- [ ] A functional file system and OS installer
+- [ ] Usermode
+- [ ] Etc..
+### In the Future
+- [ ] GUI
+- [ ] User apps
---
+++ build.sh
@@ -0,0 +1,12 @@
+nasm -f elf32 src/kernel/boot.asm -o boot.o
+clang --target=i386-elf -c src/kernel/kernel.c -o kernel.o
+x86_64-elf-ld -m elf_i386 -T src/kernel/linker.ld -o CDROOT/kernel/kernel.bin boot.o kernel.o
+rm -f boot.o kernel.o
+
+xorriso -as mkisofs -b boot/limine-bios-cd.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table \
+        --efi-boot boot/limine-uefi-cd.bin \
+        -efi-boot-part --efi-boot-image --protective-msdos-label \
+        CDROOT -o ISO/ZoneOS.iso
+
+qemu-system-x86_64 -cdrom ISO/ZoneOS.iso
